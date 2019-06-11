0efee15454c5e6f47e1eccceeef068ae78a13353
devilvole <machumph@yahoo.co.uk>
2019-06-11 18:04:05 +0100

    implemented

---
+++ assignment2-solution/app.js
@@ -0,0 +1,66 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var list = this;
+
+    list.items = ShoppingListCheckOffService.getToBuyItems();
+
+    list.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var list = this;
+
+    list.items = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyItems = 
+      [{name:'lettuce', quantity:7},
+      {name:'radishes', quantity:311},
+      {name:'tomatoes', quantity:34},
+      {name:'jar of pickles', quantity:1},
+      {name:'toblerone', quantity:11},];
+
+    var boughtItems = [];
+
+    service.addToBuyItem = function (itemName, quantity) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      toBuyItems.push(item);
+    };
+
+    // service.buyItem = function (itemIndex) {
+    //   boughtItems.push( toBuyItems.splice(itemIndex, 1));
+    // };
+
+    service.buyItem = function (itemIndex) {
+      boughtItems.push( toBuyItems[itemIndex]);
+      toBuyItems.splice(itemIndex, 1);
+    };
+
+    service.getToBuyItems = function () {
+      return toBuyItems;
+    };
+
+    service.getBoughtItems = function () {
+      return boughtItems;
+    };
+  }
+
+})();
